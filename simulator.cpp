b41dce82af7573dc18acc3ac034430ab0a36020c
Uddhav Arote <aroteuddhav@gmail.com>
2016-04-06 18:26:04 +0530

    added definition for InitialEventQueue function

--- simulator.cpp
+++ simulator.cpp
@@ -26,3 +26,22 @@ int Simulator::Start()
     return 0;
 }
 
+void Simulator::InitializeEventQueue()
+{
+    //TODO: generate random number generator object to refer to the functions
+
+    SimulationTime arrivalTime = 0.0 ;
+    //goto each and every virtual machine and call GenerateNextNumber with rng index and inverse of arrival rate
+    for (int i = 0; i < dc.totalVMs; ++i)
+    {
+        // this next statement is problematic .. need to resolve !
+        //arrivalTime = RandomNumberGenerator::GenerateNextNumber(2*i, dc.virtualmachines[i].lambda );
+
+        //-1 for newPmId
+
+        Event e = Event(arrivalTime, EventType:: REQUEST_ARRIVAL, i, dc.virtualmachines[i].hostId, -1 );
+
+        //push event to the priority queue
+        eventQueue.push(e);
+    }
+};
